7ad61a9190321a38db3460c5c4f0f91d692c06c5
Remco Visser <0908443@hr.nl>
2016-04-26 20:09:23 +0200

    Week1
    In class assignements

--- Week1/Week1/Program.cs
+++ Week1/Week1/Program.cs
@@ -10,6 +10,25 @@ namespace Week1
     {
         static void Main(string[] args)
         {
+            User user1 = new User {Name = "remco", Id = 0908443};
+            User user2 = new User {Name = "piet", Id = 00000};
+
+            Console.WriteLine("Fill in a name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Fill in a student number");
+            int id = Int32.Parse(Console.ReadLine());
+            User user3 =  new User {Name = name, Id = id};
+
+            Console.WriteLine("Fill in a name");
+            name = Console.ReadLine();
+            Console.WriteLine("Fill in a student number");
+            id = Int32.Parse(Console.ReadLine());
+            User user4 = new User { Name = name, Id = id };
+
+
+            UserAccount userAccount1 = new UserAccount("Remco", 0908443);
+            UserAccount userAccount2 = new UserAccount("Piet", 000000);
+
         }
     }
 }
---
+++ Week1/Week1/UserAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1
+{
+    class UserAccount
+    {
+        private string name;
+        private int id;
+        public UserAccount(string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+    }
+}
