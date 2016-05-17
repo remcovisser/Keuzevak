e2d915a725b3d0e158f2f5599afa47942332ce8f
Remco Visser <0908443@hr.nl>
2016-05-17 19:25:33 +0200

    extension methods

---
+++ week3/Week3/Week3/Filter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3
+{
+    public static class Filter
+    {
+        public static string Present(this int value)
+        {
+            float temp = value / 100.0f;
+            return ("$" + temp + 0).Replace(".", ",");
+        }
+
+        public static float Value(this string value)
+        {
+            return float.Parse( value.Replace("$", ""));
+        }
+    }
+}
---
+++ week3/Week3/Week3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value1 = "$41,30";
+            int value2 = 4130;
+            Console.WriteLine(value1.Value());
+            Console.WriteLine(value2.Present());
+            Console.ReadLine();
+        }
+    }
+}
