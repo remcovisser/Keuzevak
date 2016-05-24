c21a6bf5c749fd5783d4b9d59890324aaaa2a000
Remco Visser <0908443@hr.nl>
2016-05-24 19:06:23 +0200

    Week4Part1

---
+++ Week4Part1/Week4Part1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlistOfArguments("apple", "peert", "banana", "apple", "peer", "banana");
+        }
+
+        public static void AlistOfArguments(params string[] listOfFruits)
+        {
+            foreach (string fruit in listOfFruits)
+            {
+                Console.WriteLine(fruit);
+            }
+            
+        }
+    }
+}
---
+++ Week4Part1/Week4Part1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Week4Part1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week4Part1")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e4ff04c5-286e-4333-9c0f-f0b0a5e22c7f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
