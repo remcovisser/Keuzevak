79d31fee09fcc5b17d8f9c307c2e704dd17ac472
Remco Visser <0908443@hr.nl>
2016-05-24 19:33:24 +0200

    Week4Part4

--- Week4Part2/Week4Part2/Program.cs
+++ Week4Part2/Week4Part2/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace Week4Part2
 {
+    public class Fruit
+    {
+        public string Name { get; set; }
+      
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            string[] fruits = {"apple", "peer", "orange", "banana", "apple", "peer", "orange", "banana"};
+            foreach (var item in ShowMeSomeFruits(fruits))
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadLine();
+        }
+
+
+        public static IEnumerable<Fruit> ShowMeSomeFruits(string[] fruits)
+        {
+            foreach (var item in fruits)
+            {
+                if (item == "apple")
+                {
+                    yield return new Fruit { Name = "apple" };
+                }
+            }
         }
     }
 }
